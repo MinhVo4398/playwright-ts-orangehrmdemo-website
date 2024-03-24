c0dd1027de5c9e365a3a0e675cca3496143f1887
Minh Vo <minhvo4398@gmail.com>
2024-03-24 20:01:47 +0700

    finish part 12

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 This course is from Youtube: Testing mini bytes
 https://www.youtube.com/watch?v=ALzdsw798hE&list=PL9ok7C7Yn9A8UaOTZflfpvUYx1h9LwPm9&index=11
+
+1. Install .env
+   - npm i dot-env --save-dev
+2. Run test
+   ENVIRONMENT=staging npx playwright test --headed
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+import { config } from "dotenv";
 
+//staging ==>.env.staging
+if (process.env.ENVIRONMENT) {
+  config({
+    path: `.env.${process.env.ENVIRONMENT}`,
+    override: true,
+  });
+} else {
+  config();
+}
 
 export default defineConfig({
-  testDir: './e2e/tests',
+  testDir: "./e2e/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -12,19 +22,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-  
-  },
+  use: {},
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
-  
   ],
-
 });
